866157b7e7e12798d0d0940183b6235261cdec95
DallasJr <49690481+DallasJr@users.noreply.github.com>
2024-02-02 12:13:51 +0100

    open window

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"fyne.io/fyne/v2/app"
+)
 
+func main() {
+	fmt.Println("Launching app . . .")
+	a := app.New()
+	w := a.NewWindow("Groupie Tracker")
+	w.ShowAndRun()
 }
