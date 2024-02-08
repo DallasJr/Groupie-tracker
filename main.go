b5fb6175a51e61801914c29b05e3856226bee9ce
NicolasDISEP <nicolas.nd40@gmail.com>
2024-02-08 08:44:51 +0100

    lecture de donné

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"fyne.io/fyne/v2/app"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Launching app . . .")
-	a := app.New()
-	w := a.NewWindow("Groupie Tracker")
-	w.ShowAndRun()
+	// URL de l'API
+	apiURL := "https://groupietrackers.herokuapp.com/api/artists"
+
+	// Faire une requête GET à l'API
+	response, err := http.Get(apiURL)
+	if err != nil {
+		fmt.Println("Erreur lors de la requête :", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Structure pour stocker les données JSON
+	var data []map[string]interface{}
+
+	// Décoder les données JSON directement depuis la réponse
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&data)
+	if err != nil {
+		fmt.Println("Erreur lors du décodage des données JSON :", err)
+		return
+	}
+
+	// Afficher les données pour vérification
+	fmt.Println(data)
 }
